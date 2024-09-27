7eab5ef77350e0d9ae67a1ff9b3036e26104d133
Rahul Gupta <rahumanku@gmail.com>
2024-09-27 23:35:59 +0530

    Initial Commit

---
+++ BattleBucksDemo/BattleBucksDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BattleBucksDemoApp.swift
+//  BattleBucksDemo
+//
+//  Created by Satendra Maurya on 27/09/24.
+//
+
+import SwiftUI
+
+@main
+struct BattleBucksDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
